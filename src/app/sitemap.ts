39e5ba49119e43ed6fd09536e01c094ad5b809a9
William <william@jardinsurbanos.com.br>
2026-02-27 22:50:08 -0300

    feat: Add new tour page `/touren/natur-und-straende` to sitemap.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -16,5 +16,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: "monthly",
             priority: 0.8,
         },
+        {
+            url: `${baseUrl}/touren/natur-und-straende`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
     ];
 }
