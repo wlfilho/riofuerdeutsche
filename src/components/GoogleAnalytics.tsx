4d1e31115f4a563f5db19d4cfd11ab99a580b158
William <william@jardinsurbanos.com.br>
2026-04-01 23:19:32 -0300

    fix GA4

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -14,17 +14,19 @@ declare global {
 function loadGA4() {
   if (document.querySelector(`script[src*="gtag/js?id=${GA_ID}"]`)) return
 
-  const script = document.createElement('script')
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
-  script.async = true
-  document.head.appendChild(script)
-
   window.dataLayer = window.dataLayer || []
   window.gtag = function gtag(...args: unknown[]) {
     window.dataLayer.push(args)
   }
   window.gtag('js', new Date())
-  window.gtag('config', GA_ID)
+
+  const script = document.createElement('script')
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+  script.async = true
+  script.onload = () => {
+    window.gtag('config', GA_ID)
+  }
+  document.head.appendChild(script)
 }
 
 export default function GoogleAnalytics() {
